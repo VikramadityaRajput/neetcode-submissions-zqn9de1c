c638235ffba009ca01474acfa2bb78736cb1ec27
Vikramaditya Rajput <vikraj122719@gmail.com>
2026-04-24 14:52:46 -0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop(-1)
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        minimum = 2**31 - 1
+        for val in self.stack:
+            if val < minimum:
+                minimum = val
+        return minimum
